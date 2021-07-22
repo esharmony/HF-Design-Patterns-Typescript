5ec61d6c20c59d4db879f716625d03238fc9b0f7
Matthew Barnden <development.esharmony@gmail.com>
2021-07-22 16:23:03 +0100

    Removed build directory

--- strategy/build/MiniDuckSimulator/Behaviors/FlyNoWay.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var FlyNoWay = /** @class */ (function () {
-    function FlyNoWay() {
-        this.fly = function () { return console.log('I cannot fly'); };
-    }
-    return FlyNoWay;
-}());
-exports.default = FlyNoWay;
--- strategy/build/MiniDuckSimulator/Behaviors/FlyWithWings.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var FlyWithWings = /** @class */ (function () {
-    function FlyWithWings() {
-        this.fly = function () { return console.log("I'm flying!"); };
-    }
-    return FlyWithWings;
-}());
-exports.default = FlyWithWings;
